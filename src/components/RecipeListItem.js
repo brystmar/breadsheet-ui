a1cd916b96fd6a2402396f5bfdbd5ed745f640f9
Thomas <brystmar@users.noreply.github.com>
2020-03-01 17:25:14 -0800

    Delete button is less crappy

--- src/components/RecipeListItem.js
+++ src/components/RecipeListItem.js
@@ -20,10 +20,10 @@ function RecipeListItem(props) {
     return (
         <tr className="recipe-list-item" id={"recipe-list-item-" + props.id}>
             <td className="delete-recipe-button-column">
-                <button
-                    className="delete-recipe-button"
-                    onClick={handleDeleteRecipe}>-
-                </button>
+                <img alt="Delete recipe"
+                     src="https://breadsheet-public.s3-us-west-2.amazonaws.com/button_minus.png"
+                     className="delete-recipe-button"
+                     onClick={handleDeleteRecipe}/>
             </td>
             <td className="recipe-list-item-name">
                 <a href={"/recipe?id=" + props.id}>{props.name}</a>
