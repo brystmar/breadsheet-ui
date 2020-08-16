64bedf9f0cc7a9c43391c58ff3abd4b001fb5367
Thomas <brystmar@users.noreply.github.com>
2020-08-15 20:21:49 -0700

    Fixed PageTitle border, added titles to difficulty icons

--- src/components/PageTitle.js
+++ src/components/PageTitle.js
@@ -3,10 +3,11 @@ import LoadingIcon from './LoadingIcon';
 
 function PageTitle(props) {
     if (props.title === PageTitle.defaultProps.title) {
+        // TODO: Remove this once page transitions are smoother
         console.log("Loading Default Title...")
     }
     return (
-        <div className="page-title">
+        <div className={props.includeHr ? "page-title page-title-border" : "page-title"}>
             {props.title ? props.title : <LoadingIcon cssClass="loading-icon-title"/>}
         </div>
     )
